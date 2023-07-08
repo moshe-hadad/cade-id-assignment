173831bf1fba15986221c94c1bfa03ba205ba2ef
moshe.hadad <moshe.hadad@accenture.com>
2023-07-08 23:47:07 +0300

    Addin more imputing methods

--- src/case_id_assignment/imputing.py
+++ src/case_id_assignment/imputing.py
@@ -1,4 +1,5 @@
 import operator
+from collections import defaultdict
 
 import numpy as np
 import xml.etree.ElementTree as et
@@ -9,82 +10,17 @@ import case_id_assignment.utilities as util
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
-def parse_file_data_from_xml(param):
-    root = et.fromstring(param)
-    filtered = [item for item in list(root.itertext()) if item.strip()]
-    return filtered
-
-
-def impute_from_message_attributes(data_set):
-    def extract_values_message_attributes(row):
-        session_class = row['session_class']
-        message_attributes_is_str = False
-        if session_class != 'http':
-            return '', None, []
-        if isinstance(row['MessageAttributes'], str):
-            column_data = row['MessageAttributes'].replace("'", '"').replace('"1.0"', '1.0')
-            message_attributes_is_str = True
-        # print(column_data)
-        xml = None
-        starting_frame_number = ''
-        try:
-            message_attributes = simplejson.loads(column_data) if message_attributes_is_str else row[
-                'MessageAttributes']
-            starting_frame_number = message_attributes.get('http.request_in', '')
-        except simplejson.errors.JSONDecodeError:
-            index = column_data.index('"http.file_data":') + len('"http.file_data":') + 2
-            xml = column_data[index:-2].encode().decode('unicode_escape')
-
-        try:
-            xml = xml if xml else message_attributes['http.file_data']
-        except KeyError:
-            return 'KeyError'
-        xml = xml if '"1.0"' in xml else xml.replace('1.0', '"1.0"')
-        xml = xml.replace('\\xa', '')
-
-        return xml
-
-    return extract_values_message_attributes
-
-
-def _clean_xml_str(file_data):
-    # xml = file_data.replace('1.0', '"1.0"').replace('\\xa', '')
-    xml = file_data.replace('\\xa', '')
-    return xml
-
-
-def parse_message_attributes(row):
-    try:
-        message_attributes = eval(row)
-        http_type = 'http.content_type'
-        if http_type in message_attributes:
-            file_data = message_attributes['http.file_data']
-            xml = _clean_xml_str(file_data)
-            method = parse_file_data_from_xml(xml)
-        else:
-            return np.NAN
-
-    except Exception as ex:
-        pass
-    return 'test'
-
-
-class ImputeFromMessageAttributes(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        results = X['MessageAttributes'].apply(parse_message_attributes)
-        self.imputer_map = 'non'
-        return self
-
-    def transform(self, X, y=None):
-        return X
-
-
 def _extract_key_value(items):
     it = iter(items[6:])
     return dict(zip(it, it))
 
 
-PARSINGS = {'sale.order_create': _extract_key_value}
+PARSINGS = defaultdict(_extract_key_value,
+                       {'sale.order_create': _extract_key_value,
+                        'sale.order.line_create': _extract_key_value,
+                        'account.invoice_create': _extract_key_value,
+                        'account.invoice.line_create': _extract_key_value,
+                        'account.payment_create': _extract_key_value})
 
 
 def parse_null(file_data):
@@ -101,7 +37,7 @@ def parse_file_data(file_data):
             command = util.first_item(file_data)
             if command == 'execute_kw':
                 action = '_'.join(ACTION_NAME(file_data))
-                parse_method = PARSINGS.get(action, parse_null)
+                parse_method = PARSINGS[action]
                 result = parse_method(file_data)
                 return result
 
--- tests/test_imputing.py
+++ tests/test_imputing.py
@@ -9,12 +9,17 @@ import case_id_assignment.utilities as util
 from . import testutils as tu
 
 
-def test_impute_from_message_attributes():
+def _input_sample_data():
     sample_data = tu.load_sample_data(file_name='sample_for_imputing.csv')
     pipeline = Pipeline(steps=[
         ('inputter', imp.ImputeFromFileData())
     ])
     results = pipeline.fit_transform(sample_data)
+    return results
+
+
+def test_impute_sale_order_create():
+    results = _input_sample_data()
     data = {'partner_id': 1, 'partner_invoice_id': 1, 'partner_shipping_id': 1}
     indices = [7, 49, 92]
 
@@ -22,3 +27,53 @@ def test_impute_from_message_attributes():
     expected = pd.DataFrame(data, index=indices, dtype=float)
 
     assert_frame_equal(actual, expected)
+
+
+def test_impute_sale_order_line_create():
+    results = _input_sample_data()
+
+    data = {'order_id': [94., 103.], 'product_id': [21., 21.], 'name': ['Conference Chair', 'Conference Chair'],
+            'product_uom_qty': [1., 3.], 'price_unit': [5., 4.]}
+    indices = [20, 62]
+
+    actual = results.loc[indices, list(data.keys())]
+    expected = pd.DataFrame(data, index=indices)
+
+    assert_frame_equal(actual, expected)
+
+
+def test_impute_account_invoice_create():
+    results = _input_sample_data()
+    data = {'name': ['Cabinet with Doors_203', 'Acoustic Bloc Screens_173'],
+            'partner_id': [1243., 1220.], 'currency_id': [2., 2.], 'origin': ['PO00203', 'PO00173']}
+    indices = [194, 246]
+
+    actual = results.loc[indices, list(data.keys())]
+    expected = pd.DataFrame(data, index=indices)
+
+    assert_frame_equal(actual, expected)
+
+
+def test_impute_account_invoice_line_create():
+    results = _input_sample_data()
+    data = {'invoice_id': [38., 39.], 'name': ['Conference Chair', 'Office Chair'],
+            'price_unit': [5., 3.], 'quantity': [1., 3.], 'purchase_line_id': [163., 167.], 'account_id': [24., 24.]}
+    indices = [188, 199]
+
+    actual = results.loc[indices, list(data.keys())]
+    expected = pd.DataFrame(data, index=indices)
+
+    assert_frame_equal(actual, expected)
+
+
+def test_impute_account_payment_create():
+    results = _input_sample_data()
+    data = {'payment_type': ['outbound'], 'payment_method_id': [2.], 'amount': [1.],
+            'currency_id': [2.], 'journal_id': [2.]}
+    indices = [196]
+
+    actual = results.loc[indices, list(data.keys())]
+    expected = pd.DataFrame(data, index=indices)
+
+    assert_frame_equal(actual, expected)
+
